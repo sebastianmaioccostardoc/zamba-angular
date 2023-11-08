08f21c654059508c67fba0df272e1a3884c6042f
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2023-11-08 11:32:39 -0300

    Bindings inserted

--- src/app/routes/pro/account/settings/security/security.component.ts
+++ src/app/routes/pro/account/settings/security/security.component.ts
@@ -5,16 +5,72 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-account-settings-security',
   templateUrl: './security.component.html',
-  styles: [
-    `
+  styles: [`
       nz-select {
         margin: 0 8px 10px 0;
         width: 200px;
       }
-    `
-  ],
+
+    `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
+// export class FamilyData {
+//   private _CuitEmpresa: number;
+//   private _Legajo: string;
+//   private _ApellidoNombre: string;
+//   private _CuilCuit: string;
+//   private _Nombre: string;
+//   private _Apellido: string;
+//   private _FechaNacimiento: Date;
+//   private _Nacionalidad: string;
+//   private _TipoDocumento: string;
+//   private _NumeroDocumento: string;
+//   private _Genero: string;
+//   private _EstadoCivil: string;
+//   private _Pais: string;
+//   private _Provincia: string;
+//   private _Localidad: string;
+//   private _Barrio: string;
+//   private _Municipio: string;
+//   private _DepartamentoProvincial: string;
+//   private _Calle: string;
+//   private _Altura: string;
+//   private _BeneficioSeguroDeVida: string;
+//   private _PersonaACargo: string;
+//   private _ProcentajeDeduccionGanancia: number;
+
+//   constructor() {
+//   }
+// }
+
 export class ProAccountSettingsSecurityComponent {
   constructor(public msg: NzMessageService) { }
+  // MyFamilyData = new FamilyData();
+
+  Family: any = {
+    CuitEmpresa: 0,
+    Legajo: "",
+    ApellidoNombre: "",
+    CuilCuit: "",
+    Nombre: "",
+    Apellido: "",
+    FechaNacimiento: Date.now(),
+    Nacionalidad: "",
+    TipoDocumento: "",
+    NumeroDocumento: "",
+    Genero: "Male",
+    EstadoCivil: "Single",
+    Pais: "Argentina",
+    Provincia: "Buenos Aires",
+    Localidad: "",
+    Barrio: "",
+    Municipio: "",
+    DepartamentoProvincial: "",
+    Calle: "",
+    Altura: 0,
+    BeneficioSeguroDeVida: "",
+    PersonaACargo: "",
+    ProcentajeDeduccionGanancia: 0
+  };
 }
