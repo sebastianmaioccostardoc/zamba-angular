f0f86614d2d614d346f672926f02e89c4fb4a551
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2024-02-08 17:23:50 -0300

    Enviroments.prod.ts completed with ApiRestBasePath and apiWebViews

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,9 @@ import { Environment } from '@delon/theme';
 export const environment = {
   production: true,
   useHash: true,
+  apiRestBasePath: "ZambaWeb.RestApi/api/Dashboard",
+  apiWebViews: "Zamba.Web/Views",
+  cliente: 'zamba',
   api: {
     baseUrl: './',
     refreshTokenEnabled: true,
