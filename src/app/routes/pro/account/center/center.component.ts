f5657e3f8ad8a53ddaca01f8dd5a6f2208bbe143
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2024-04-24 16:33:54 -0300

    EA-RRHH-ZT317: Update Center.component and added class employeeUser

--- src/app/routes/pro/account/center/center.component.ts
+++ src/app/routes/pro/account/center/center.component.ts
@@ -3,84 +3,62 @@ import { ActivationEnd, Router } from '@angular/router';
 import { _HttpClient } from '@delon/theme';
 import { Subscription, zip, filter } from 'rxjs';
 
+import { employeeUser } from './entitie/employeeUser';
+
 @Component({
   selector: 'app-account-center',
   templateUrl: './center.component.html',
   styleUrls: ['./center.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProAccountCenterComponent implements OnInit, OnDestroy {
+export class ProAccountCenterComponent implements OnInit {
+  user: employeeUser = {
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png', // Employee Photo
+
+    name: 'Name',
+    lastName: 'LastName',
+    email: 'Email@Mail.Ail',
+    phone: '1100002222',
+    birthday: 'Birthday',
+
+    password: 'Password',
+
+
+    employmentStatus: 'Employment Status',
+    area: 'Area',
+    department: 'Department',
+    position: 'Position',
+
+    workMode: 'Work Mode',
+
+    /* 
+      Si trabajo con metodología Presencial: mi Ubicación Laboral.
+      Si trabajo Hibrido: La Ubicación Laboral y los días que me encuentro ahí.
+      Si trabajo Remoto: Desde que País, Ciudad, Estado cual es la diferencia horaria  y cual es el horario de trabajo
+    */
+
+    workLocation: 'Work Location',
+    country: 'Country',
+    city: 'City',
+    state: 'State',
+    timeDifference: 'Time Difference',
+    workSchedule: 'Work Schedule',
+
+  };
+
   constructor(
     private router: Router,
     private http: _HttpClient,
     private cdr: ChangeDetectorRef
-  ) {}
-  private router$!: Subscription;
-  @ViewChild('tagInput', { static: false }) private tagInput!: ElementRef<HTMLInputElement>;
-  user: any;
-  notice: any;
-  tabs = [
-    {
-      key: 'articles',
-      tab: '文章 (8)'
-    },
-    {
-      key: 'applications',
-      tab: '应用 (8)'
-    },
-    {
-      key: 'projects',
-      tab: '项目 (8)'
-    }
-  ];
-  pos = 0;
-  taging = false;
-  tagValue = '';
-
-  private setActive(): void {
-    const key = this.router.url.substring(this.router.url.lastIndexOf('/') + 1);
-    const idx = this.tabs.findIndex(w => w.key === key);
-    if (idx !== -1) {
-      this.pos = idx;
-    }
-  }
+  ) { }
 
   ngOnInit(): void {
-    zip(this.http.get('/user/current'), this.http.get('/api/notice')).subscribe(([user, notice]) => {
-      this.user = user;
-      this.notice = notice;
+    this.http.get('/account/dashboard/getEmployeeUser').subscribe((res: any) => {
+      this.user = res;
+
       this.cdr.detectChanges();
     });
-    this.router$ = this.router.events.pipe(filter(e => e instanceof ActivationEnd)).subscribe(() => this.setActive());
-    this.setActive();
+
   }
 
-  to(item: { key: string }): void {
-    this.router.navigateByUrl(`/pro/account/center/${item.key}`);
-  }
-  tagShowIpt(): void {
-    this.taging = true;
-    this.cdr.detectChanges();
-    this.tagInput.nativeElement.focus();
-  }
-
-  tagBlur(): void {
-    const { user, cdr, tagValue } = this;
-    if (tagValue && user.tags.filter((tag: { label: string }) => tag.label === tagValue).length === 0) {
-      user.tags.push({ label: tagValue });
-    }
-    this.tagValue = '';
-    this.taging = false;
-    cdr.detectChanges();
-  }
-
-  tagEnter(e: KeyboardEvent): void {
-    if (e.key === 'Enter') {
-      this.tagBlur();
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.router$.unsubscribe();
-  }
 }
