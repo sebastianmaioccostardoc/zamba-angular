c526a98a609b414e95d1d571758335763cebc55a
NicolasCabrera <nicolas.cabrera@stardoc.com.ar>
2024-04-26 12:30:24 -0300

    NC: servicio para escuchar mensajes de formularios de zamba

--- src/app/layout/basic/basic.component.ts
+++ src/app/layout/basic/basic.component.ts
@@ -4,6 +4,7 @@ import { LayoutDefaultOptions } from '@delon/theme/layout-default';
 import { environment } from '@env/environment';
 
 import { ZambaService } from '../../services/zamba/zamba.service';
+import { MessageListenerService } from 'src/app/services/message-listener.service';
 
 @Component({
   selector: 'layout-basic',
@@ -13,12 +14,16 @@ import { ZambaService } from '../../services/zamba/zamba.service';
 export class LayoutBasicComponent implements OnInit {
   constructor(
     private settings: SettingsService,
-    private ZambaService: ZambaService
-  ) {}
+    private ZambaService: ZambaService,
+    private messageListener: MessageListenerService
+  ) { }
   ngOnInit(): void {
     this.ZambaService.GetSidebarItems();
     this.settings.setLayout('collapsed', true);
+    this.messageListener.startListening();
   }
+
+
 
   options: LayoutDefaultOptions = {
     logoExpanded: `./assets/logo-zamba-rrhh-t.svg`,
